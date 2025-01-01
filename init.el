8d5002cdad0becf0719ad7fc6cdfbe138ae3e863
Yutaka Ichibangase <yichiban@gmail.com>
2025-01-01 16:50:20 +0900

    simplified

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+;;; init.el --- initialization
+;;; Commentary:
+
+;;; Code:
+
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
 ;; \C-h to backspace
@@ -7,7 +12,7 @@
 (load "ichiban.el")
 
 ;; Dynamic Macro
-(defconst *dmacro-key* "\C-t" "繰返し指定キー")
+(defconst *dmacro-key* "\C-t" "繰返し指定キー.")
 (global-set-key *dmacro-key* 'dmacro-exec)
 (autoload 'dmacro-exec "dmacro" nil t)
 
@@ -29,9 +34,7 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-(dolist (package '(color-theme
-		   zenburn-theme
-		   auto-complete
+(dolist (package '(auto-complete
 		   flycheck
 		   rust-mode
 		   merlin
@@ -39,90 +42,13 @@
 		   go-errcheck
 		   go-autocomplete
 		   go-eldoc
-		   gotest))
+		   gotest
+		   ellama
+		   zenburn-theme))
   (unless (package-installed-p package)
     (package-install package)))
 
-;; color-theme
-(require 'color-theme)
-(color-theme-initialize)
-(require 'zenburn-theme)
+(load-theme 'zenburn t)
 
-;; css
-(setq css-indent-offset 2)
-
-;; js
-(setq js-indent-level 2)
-
-;; rust
-(require 'rust-mode)
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
-
-;; common lisp
-(setq slime-helper-file (expand-file-name "~/quicklisp/slime-helper.el"))
-(when (file-exists-p slime-helper-file)
-    (load slime-helper-file))
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
-(put 'downcase-region 'disabled nil)
-(put 'narrow-to-region 'disabled nil)
-
-;; OCaml
-(setq tuareg-site-file
-      "~/.opam/system/share/emacs/site-lisp/tuareg-site-file")
-(when (file-exists-p tuareg-site-file)
-  (load tuareg-site-file))
-(when (file-exists-p "~/.opam")
-  (add-to-list 'load-path "~/.opam/system/share/emacs/site-lisp/")
-  ;; Add opam emacs directory to the load-path
-  (setq opam-share
-	(substring
-	 (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
-  (add-to-list 'load-path (concat opam-share "/emacs/site-lisp")))
-;; Load merlin-mode
-(require 'merlin)
-;; Start merlin on ocaml files
-(add-hook 'tuareg-mode-hook 'merlin-mode t)
-(add-hook 'caml-mode-hook 'merlin-mode t)
-;; Enable auto-complete
-(setq merlin-use-auto-complete-mode 'easy)
-;; Use opam switch to lookup ocamlmerlin binary
-(setq merlin-command 'opam)
-;; indentations
-(setq tuareg-indent-align-with-first-arg nil)
-(put 'upcase-region 'disabled nil)
-
-;; Golang
-(require 'go-mode)
-(add-hook 'before-save-hook 'gofmt-before-save)
-;; go-errcheck
-(require 'go-errcheck)
-;; gocode
-(require 'go-autocomplete)
-(require 'auto-complete-config)
-(ac-config-default)
-;; go-eldoc
-(require 'go-eldoc)
-(add-hook 'go-mode-hook 'go-eldoc-setup)
-;; go-guru
-(defvar go-guru-file
-  (substitute-in-file-name
-   "$GOPATH/src/golang.org/x/tools/cmd/guru/go-guru.el"))
-(when (file-exists-p go-guru-file)
-  (load go-guru-file)
-  (add-hook 'go-mode-hook 'go-guru-mode))
-;; gotest
-(require 'gotest)
-(defun my-go-mode-hook-for-go-test ()
-  (define-key go-mode-map (kbd "C-x f") 'go-test-current-file)
-  (define-key go-mode-map (kbd "C-x t") 'go-test-current-test)
-  (define-key go-mode-map (kbd "C-x p") 'go-test-current-project)
-  (define-key go-mode-map (kbd "C-x x") 'go-run)
-  (dolist (elt go-test-compilation-error-regexp-alist-alist)
-    (add-to-list 'compilation-error-regexp-alist-alist elt))
-  (dolist (elt (reverse go-test-compilation-error-regexp-alist))
-    (add-to-list 'compilation-error-regexp-alist elt t)))
-(add-hook 'go-mode-hook 'my-go-mode-hook-for-go-test)
-
-;; Flycheck
-(require 'flycheck)
-(global-flycheck-mode)
+(provide 'init)
+;;; init.el ends here
--- lisp/ichiban.el
+++ lisp/ichiban.el
@@ -1,134 +1,23 @@
 ;;; ichiban.el --- my own utilities
+;;; Commentary:
 
 ;; Copyright (C) 2010  ICHIBANGASE, Yutaka
 
 ;; Author: ICHIBANGASE, Yutaka <yichiban@gmail.com>
 ;; Keywords: utils
 
-(require 'cl)
+;;; Code:
 
-(defmacro* narrow-if-needed (&body body)
-  "transient-mark-modeだった場合はbodyのオペレーションがリージョン内に制限される"
-  (lexical-let ((needed (gensym)))
-    `(lexical-let ((,needed (and transient-mark-mode mark-active))
-		   result)
-       (when ,needed (narrow-to-region (region-beginning) (region-end)))
-       (setq result (progn ,@body))
-       (when ,needed (widen))
-       result)))
-
-(defun* mark-whole-buffer-if-needed ()
-  "transient-mark-modeでない場合はバッファ全体がマークされます"
-  (lexical-let ((needed (not (and transient-mark-mode mark-active))))
-    (if needed (mark-whole-buffer))))
-
-(lexical-let ((n 0))
-  (defun* tmp ()
-    "一時バッファを生成する"
+(let ((n 0))
+  (defun tmp ()
+    "一時バッファを生成する."
     (interactive)
     (switch-to-buffer
      (get-buffer-create (concat "tmp" (int-to-string n))))
-    (incf n)))
-
-(defun* uniq ()
-  "uniqコマンドに相当する。transient-mark-modeだった場合はリージョンに対してのみ作用する"
-  (interactive)
-  (narrow-if-needed
-   (beginning-of-buffer)
-   (while (re-search-forward "\\(.*\n\\)\\1+" nil t)
-     (replace-match "\\1" nil nil))))
-
-(defun* mecab (&optional (option "-Owakati"))
-  "mecabを用いて形態素解析する。transient-mark-modeだった場合はリージョンに対してのみ作用する"
-  (interactive)
-  (mark-whole-buffer-if-needed)
-  (lexical-let ((b (region-beginning))
-		(e (region-end)))
-    (call-process-region b e "mecab" t t nil option))
-  (delete-backward-char 2))
-
-(defun* growl (msg)
-  "growlnotifyでGrowlにメッセージを表示する"
-  (interactive "sMessage:")
-  (shell-command
-   (format "growlnotify -m \"%s\"" msg)))
-
-(defun* hankaku ()
-  "携帯用に対応する半角文字があれば置換する"
-  (interactive)
-  (narrow-if-needed
-   (beginning-of-buffer)
-   (while (re-search-forward "\[ァ-ヶー、。？！―・　（）／「」“”０-９Ａ-Ｚ\]+" nil t)
-     (japanese-hankaku-region (match-beginning 0) (match-end 0)))))
-
-(defun* censored ()
-  "検閲文字列を挿入"
-  (interactive)
-  (insert "<censored>"))
-
-(defun* tweet (status)
-  (twittering-call-api 'update-status `((status . ,status))))
-
-(defun* rand-8-digits ()
-  ""
-  (interactive)
-  (insert (number-to-string (random 10)))
-  (insert (number-to-string (random 10)))
-  (insert (number-to-string (random 10)))
-  (insert (number-to-string (random 10)))
-  (insert (number-to-string (random 10)))
-  (insert (number-to-string (random 10)))
-  (insert (number-to-string (random 10)))
-  (insert (number-to-string (random 10))))
-
-(defun* jsonpp (&optional b e)
-  "JSON Pretty Pringing"
-  (interactive "r")
-  (shell-command-on-region b e "jsonpp"))
-
-(defun base64 (&optional b e)
-  "base64 encode/decode"
-  (interactive "r")
-  (shell-command-on-region b e "ruby -rbase64 -e 's = $stdin.gets; print Base64.send(/^[A-Za-z0-9\\+\\/=]+$/ =~ s ? :decode64 : :encode64, s).split($/).join'" nil t))
-
-(defun tsv-to-sexp (&optional b e)
-  "TSV to S-exp"
-  (interactive "r")
-  (lexical-let ((str (buffer-substring b e)))
-    (delete-region b e)
-    (insert (pprint (mapcar (lambda (line) (split-string line "\t"))
-			    (split-string str "\n"))))))
-
-(defun pprint (form &optional output-stream)
-  "pretty print S-exp found at http://stackoverflow.com/a/3552347"
-  (princ (with-temp-buffer
-           (cl-prettyprint form)
-           (buffer-string))
-         output-stream))
-
-(defun tsv-to-ruby (&optional b e)
-  "TSV to ruby"
-  (interactive "r")
-  (lexical-let ((str (buffer-substring b e)))
-    (delete-region b e)
-    (insert (mapconcat #'identity
-		       (list "["
-			     (mapconcat #'tsv-to-ruby-line (split-string str "\n") ",\n ")
-			     "]")
-		       ""))))
-
-(defun tsv-to-ruby-line (line)
-  (mapconcat #'identity
-	     (list "["
-		   (mapconcat #'tsv-to-ruby-elem (split-string line "\t") ", ")
-		   "]")
-	     ""))
-
-(defun tsv-to-ruby-elem (elem)
-  (format "%S" elem))
+    (setq n (+ n 1))))
 
 (defun flow ()
-  "toggle full-screen"
+  "Toggle full-screen."
   (interactive)
   (if (fboundp 'ns-toggle-fullscreen)
       (ns-toggle-fullscreen); carbon emacs has this feature
@@ -138,3 +27,6 @@
   
 (global-set-key [f11] 'flow)
 (add-hook 'after-make-frame-functions 'flow)
+
+(provide 'ichiban)
+;;; ichiban.el ends here
